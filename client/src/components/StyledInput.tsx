63db84ce4cb400f40dcb144fc6ebead3a0e77b79
manuelMarkDenver <manuel.markdenver@gmail.com>
2024-05-26 10:53:05 +0800

    refactored styled input

--- client/src/components/StyledInput.tsx
+++ client/src/components/StyledInput.tsx
@@ -1,29 +1,17 @@
 import { Form, Stack } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { selectSearchString } from "../slices/photosSlice";
+import { useDispatch } from "react-redux";
 import { setPhotoSearchString } from "../slices/photosSlice";
 import debounce from "just-debounce-it";
-import { useEffect, useState } from "react";
-// import { usePhotos } from "../features/photos/shared/usePhotos";
 
 const StyledInput: React.FC = () => {
-  const [searchString, setSearchString] = useState<string>("");
-
-  const globalSearchString = useSelector(selectSearchString);
-
-  useEffect(() => {
-    if (globalSearchString) setSearchString(globalSearchString);
-  }, [globalSearchString]);
-
   const dispatch = useDispatch();
 
   const debounceSearch = debounce((value: string) => {
     dispatch(setPhotoSearchString(value.trim()));
-  }, 1500);
+  }, 700);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setSearchString(value);
     debounceSearch(value);
   };
 
@@ -33,7 +21,6 @@ const StyledInput: React.FC = () => {
         <Form.Control
           type="text"
           placeholder="Search images by title..."
-          value={searchString}
           onChange={handleChange}
         />
       </Stack>
